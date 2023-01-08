e70df1db9f37be3ef2c7d80cd65339340fde0212
Agil_Alatas <agilalatas17@gmail.com>
2023-01-09 00:08:01 +0700

    first commit

---
+++ src/elements/Button/index.test.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+// import komponen yang akan di testing
+import Button from ".";
+
+// tidak bisa menekan tombol jika memiliki atribut "isDisabled"
+test("Should not allow click button if isDisabled is present", () => {
+  const { container } = render(<Button isDisabled></Button>);
+
+  expect(container.querySelector("span.disabled")).toBeInTheDocument();
+});
+
+test("should render loading/spinner", () => {
+  const { container, getByText } = render(<Button isLoading></Button>);
+
+  expect(getByText(/loading/i)).toBeInTheDocument();
+  expect(container.querySelector("span")).toBeInTheDocument();
+});
+
+test("Should render <a> tag", () => {
+  const { container } = render(<Button type="link" isExternal></Button>);
+
+  expect(container.querySelector("a")).toBeInTheDocument();
+});
+
+test("Should render <a> tag", () => {
+  const { container } = render(<Button type="link" isExternal></Button>);
+
+  expect(container.querySelector("a")).toBeInTheDocument();
+});
+
+test("Should render <Link> Component", () => {
+  const { container } = render(
+    <Router>
+      <Button href="" type="link"></Button>
+    </Router>
+  );
+
+  expect(container.querySelector("a")).toBeInTheDocument();
+});
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Button from "elements/Button";
+
+class LandingPage extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default LandingPage;
